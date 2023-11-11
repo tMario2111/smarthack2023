793defe8a7c8ac375add7d58dcd3665151908c96
GodGivenn <balicabogdaniulian@gmail.com>
2023-11-11 17:08:56 +0200

    added assets and navbar

--- lib/router/home_route.dart
+++ lib/router/home_route.dart
@@ -20,6 +20,50 @@ class _HomeRouteState extends State<HomeRoute> {
   @override
   Widget build(BuildContext context) {
     test();
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('School Buddy'),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.purple,
+              ),
+              child: Text(
+                'Meniu',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              title: Text('Dashboard'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('My Progress'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Feedback'),
+              onTap: (){
+                Navigator.pop(context);
+              }
+            ),
+          ],
+        ),
+      ),
+      body: Center(
+        child: Text('Your main content goes here'),
+      ),
+    );
   }
 }
