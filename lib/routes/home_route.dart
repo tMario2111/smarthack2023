37b7f1745f299120d6b0ee2a439cab8c64a89564
= <neagumario21@yahoo.com>
2023-11-11 22:04:11 +0200

    Codul lui Bogdan

--- lib/routes/home_route.dart
+++ lib/routes/home_route.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-
-import 'package:pocketbase/pocketbase.dart';
-
-import '../pb_instance.dart';
+import 'package:intl/intl.dart';
 
 import '../widgets/side_drawer.dart';
 
@@ -16,14 +13,87 @@ class HomeRoute extends StatefulWidget {
 class _HomeRouteState extends State<HomeRoute> {
   @override
   Widget build(BuildContext context) {
+    var now = DateTime.now();
+
+    String formattedDate = DateFormat.yMMMMd().format(now).toString();
+    String userName = 'YourName';
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Dashboard'),
       ),
       drawer: sideDrawer(context),
-      body: const Center(
-        child: Text('Home Route'),
+      body: Center(
+        child: Container(
+          margin: const EdgeInsets.only(
+            bottom: 250,
+          ),
+          width: MediaQuery.of(context).size.width * 0.8,
+          height: MediaQuery.of(context).size.width * 0.2,
+          padding: const EdgeInsets.only(left: 35, top: 25),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            gradient: LinearGradient(
+              colors: [Colors.purple[500]!, Colors.purple[100]!],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: Row(
+            children: [
+              // Partea stângă a containerului
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Date in the top left corner
+                  Text(
+                    formattedDate,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 100),
+                  Text(
+                    'Welcome back, $userName!',
+                    style: const TextStyle(
+                      fontSize: 60,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                  const Text(
+                    'Always stay updated in your student portal',
+                    style: TextStyle(
+                      color: Color.fromARGB(158, 255, 255, 255),
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              // Partea dreaptă a containerului cu imaginea
+              Expanded(
+                child: Container(
+                  padding: const EdgeInsets.only(right: 35),
+                  alignment: Alignment.centerRight,
+                  child: Image.asset(
+                    '../../assets/welcome_student.png',
+                    width: 300,
+                    height: 300,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
+}
+
+void main() {
+  runApp(const MaterialApp(
+    home: HomeRoute(),
+  ));
 }
